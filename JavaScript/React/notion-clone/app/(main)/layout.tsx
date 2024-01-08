a9f9eef716699083b6f7b837d611c1b449a72239
asdf1596 <7082mc@naver.com>
2024-01-08 22:26:52 +0900

    1:56:31

--- JavaScript/React/notion-clone/app/(main)/layout.tsx
+++ JavaScript/React/notion-clone/app/(main)/layout.tsx
@@ -1,20 +1,31 @@
 "use client";
+
 import { Spinner } from "@/components/Spinner";
 import { useConvexAuth } from "convex/react";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
+import { Navigation } from "./(routes)/_components/Navigation";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
     const { isAuthenticated, isLoading } = useConvexAuth();
+
     if (isLoading) {
         return (
-            <div className="h-full flex items-cecnter justify-center">
+            <div className="h-full flex items-center justify-center">
                 <Spinner size="lg" />
             </div>
         );
     }
+
     if (!isAuthenticated) {
         return redirect("/");
     }
-    return <div>{children}</div>;
+
+    return (
+        <div className="h-full flex dark:bg-[#1F1F1F]">
+            <Navigation />
+            <main className="flex-1 h-full overflow-y-auto">{children}</main>
+        </div>
+    );
 };
 
 export default MainLayout;
