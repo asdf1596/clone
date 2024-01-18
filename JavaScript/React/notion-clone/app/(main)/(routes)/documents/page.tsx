694f0dfba237cddbad98aaa8629b60e74aaf052e
asdf1596 <7082mc@naver.com>
2024-01-18 21:33:14 +0900

    20240118

--- JavaScript/React/notion-clone/app/(main)/(routes)/documents/page.tsx
+++ JavaScript/React/notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
 const DocumentsPage = () => {
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
