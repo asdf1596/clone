cd9fc1ccc51506acc88909f3e4435370e030a2a3
asdf1596 <7082mc@naver.com>
2024-01-22 20:21:38 +0900

    20240122-02:56:11

--- JavaScript/React/notion-clone/app/(main)/_components/navigation.tsx
+++ JavaScript/React/notion-clone/app/(main)/_components/navigation.tsx
@@ -7,10 +7,13 @@ import { isAbsolute } from "path";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
+    const documents = useQuery(api.documents.get);
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null);
@@ -121,7 +124,9 @@ export const Navigation = () => {
                     </p>
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>{document.title}</p>
+                    ))}
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
