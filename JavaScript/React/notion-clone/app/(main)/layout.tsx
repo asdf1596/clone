c6a7772b3b7a5db4151ac4eceb4c0fff09d90061
asdf1596 <7082mc@naver.com>
2024-01-28 18:05:10 +0900

    20240128

--- JavaScript/React/notion-clone/app/(main)/layout.tsx
+++ JavaScript/React/notion-clone/app/(main)/layout.tsx
@@ -10,7 +10,11 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
     if (isLoading) {
         return (
-            <div className="h-full flex items-center justify-center">
+            <div
+                className="h-full 
+                            flex items-center 
+                            justify-center"
+            >
                 <Spinner size="lg" />
             </div>
         );
